74da24822b99245b97cb15775d7928b2e4243b37
pvivek629 <pvivek629@gmail.com>
2023-09-19 18:31:21 +0530

    node server and ui changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// server.js
+
 
 const express = require('express');
 const cors = require('cors');
@@ -10,14 +10,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Define an API endpoint for fetching images from Unsplash
+
 app.get('/api/images', async (req, res) => {
   try {
     const { query, page } = req.query;
-    const perPage = 10; // Number of images per page
-    const accessKey = 'foW53BDc17GpHshU5wvheO-Af884fYpcgrn_iGEmXN0'; // Replace with your Unsplash access key
+    const perPage = 10;
+    const accessKey = 'foW53BDc17GpHshU5wvheO-Af884fYpcgrn_iGEmXN0'; 
 
-    // Make a request to the Unsplash API
+    
     const apiUrl = `https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}&per_page=${perPage}&query=${query}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
